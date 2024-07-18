cc79f4d29c275bbb34e9ab717c48cf6a1ce0836a
Elijah Wright <elw@stderr.org>
2024-07-18 15:06:01 -0500

    Add zsh-nix-shell plugin to zsh.

--- home/shells.nix
+++ home/shells.nix
@@ -79,6 +79,16 @@
           rev = "afaf2965b41fdc6ca66066e09382726aa0b6aa04";
           sha256 = "1s23azd9hk57dgya0xrqh16jq1qbmm0n70x32mxg8b29ynks6w8n";
         };
+      },
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.8.0";
+          sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+        };
       }
     ];
 
