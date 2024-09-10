47180c69c0ab6a3426151f3eff3294dd64b00290
Elijah Wright <elw@stderr.org>
2024-09-10 15:13:58 -0500

    Bugfix?

--- alfred.nix
+++ alfred.nix
@@ -1,14 +1,14 @@
-{ ... }:
-{
+#{ ... }:
+#{
 
 alfred.includeOverlay
-//  
-//  nixpkgs.overlays = [ alfred.overlays.default ];
-//  environment.systemPackages = with pkgs; [
-//    alfredGallery.spotify-mini-player
-// //   alfredGallery.emoji-search
-//    unzip
-//  ];
+  
+#  nixpkgs.overlays = [ alfred.overlays.default ];
+#  environment.systemPackages = with pkgs; [
+#    alfredGallery.spotify-mini-player
+#   alfredGallery.emoji-search
+#    unzip
+#  ];
 
 alfred.darwinModules.activateWorkflows
-}
+#}
