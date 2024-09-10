586d0964dbe346809f9d74c559241016f0ba217e
Elijah Wright <elw@stderr.org>
2024-09-10 14:50:32 -0500

    Update alfred.nix.

--- alfred.nix
+++ alfred.nix
@@ -1,11 +1,12 @@
-
-        ({ config, pkgs, ... }: {
-          nixpkgs.overlays = [ alfred.overlays.default ];
-          environment.systemPackages = with pkgs; [
-            alfredGallery.spotify-mini-player
-            alfredGallery.emoji-search
-            unzip
-          ];
-        })
-        alfred.darwinModules.activateWorkflows
-
+{ ... }:
+{
+  ({ config, pkgs, ... }: {
+  nixpkgs.overlays = [ alfred.overlays.default ];
+  environment.systemPackages = with pkgs; [
+    alfredGallery.spotify-mini-player
+    alfredGallery.emoji-search
+    unzip
+  ];
+})
+alfred.darwinModules.activateWorkflows
+}
