be6a9ae37d198ed4299804a177c4d8bb1efc0ea9
Elijah Wright <elw@stderr.org>
2025-08-06 11:28:50 -0500

    More fixes.

--- home/default.nix
+++ home/default.nix
@@ -4,22 +4,10 @@
   # Allow unfree packages like VSCode and its extensions
   nixpkgs.config.allowUnfree = true;
 
-  # https://nix-community.github.io/home-manager/index.html#sec-usage-configuration
-  #
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
-
   # https://nix-community.github.io/home-manager/options.html#opt-home.stateVersion
   home.stateVersion = "23.11";
 
   # Let Home Manager install and manage itself.
-  # https://nix-community.github.io/home-manager/options.html#opt-programs.home-manager.enable
   programs.home-manager.enable = true;
 
   # Home Manager needs a bit of information about you and the
@@ -27,16 +15,15 @@
   home.username = "elw";
   home.homeDirectory = "/Users/elw";
 
-  # Import other home-manager modules
+  # Only import files that actually exist - comment out missing ones
   imports = [
-    ./vscode.nix
-    ./shell.nix
-    ./git.nix
-    ./assets.nix # Symlinked assets configurations
-    ./packages.nix # Packages that are not included in `nix-darwin` and `home-manager`
-    ./shells.nix # Shell configurations
-    ./git.nix # Git configurations
-    ./dotfiles.nix # Dotfiles management (optional)
-    ../vscode-extensions.nix # VSCode extensions and configuration
+    # ./vscode.nix           # Create this file or comment out
+    # ./shell.nix            # Create this file or comment out  
+    # ./git.nix              # Create this file or comment out
+    # ./assets.nix           # Create this file or comment out
+    # ./packages.nix         # Create this file or comment out
+    # ./shells.nix           # Create this file or comment out
+    # ./dotfiles.nix         # Create this file or comment out
+    ../vscode-extensions.nix # Only if this file exists
   ];
 }
